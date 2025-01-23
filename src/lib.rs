c9a3e209710200fa11b2cf00111b12c6367528a0
Nikita Kudravcev <nikita@okic.ru>
2025-01-24 02:20:40 +0300

    allow specifying threshold

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,19 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::parse::{Error, Parse, ParseStream, Result};
+use syn::{parse_macro_input, ExprLit, ItemFn, Lit};
 
 #[proc_macro_attribute]
 pub fn time_me(attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemFn);
-    if !attr.is_empty() {
-        return quote! {
-            compiler_error!("`time_me` must be used without arguments");
-            #input
-        }
-        .into();
-    }
+    let thresh = if attr.is_empty() {
+        core::time::Duration::from_millis(50)
+    } else {
+        parse_macro_input!(attr as Dur).0
+    };
+    let thr_sec = thresh.as_secs();
+    let thr_nan = thresh.subsec_nanos();
 
     let ItemFn {
         sig,
@@ -34,7 +35,7 @@ pub fn time_me(attr: TokenStream, item: TokenStream) -> TokenStream {
             impl ::core::ops::Drop for PerfGuard {
                 fn drop(&mut self) {
                     let elapsed = self.0.elapsed();
-                    if (elapsed > ::core::time::Duration::MILLISECOND*50) {
+                    if (elapsed > ::core::time::Duration::new(#thr_sec, #thr_nan)) {
                         ::log::debug!("perf: {} took {:?}", stringify!(#function_identifier), elapsed);
                     }
                 }
@@ -45,4 +46,26 @@ pub fn time_me(attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     )
     .into()
+}
+
+struct Dur(core::time::Duration);
+
+impl Parse for Dur {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let ExprLit { attrs, lit } = ExprLit::parse(input)?;
+        if !attrs.is_empty() {
+            return Err(Error::new(lit.span(), "unexpected attributes"));
+        }
+        let Lit::Int(lit) = lit else {
+            return Err(Error::new(lit.span(), "unexpected literal: expected Int"));
+        };
+        if lit.suffix() != "ms" {
+            return Err(Error::new(
+                lit.span(),
+                format_args!("unexpected literal suffix: {}, expected `ms`", lit.suffix()),
+            ));
+        }
+        let ms: u64 = lit.base10_parse()?;
+        Ok(Self(core::time::Duration::from_millis(ms)))
+    }
 }
